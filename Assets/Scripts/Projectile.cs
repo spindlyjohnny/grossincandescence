e82efd29fae7c36236e34655835566f982d5c50f
HelVal1010 <valenciayonghel@gmail.com>
2023-08-20 20:24:06 +0800

    mage projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -12,9 +12,16 @@ public class Projectile : Weapon
     public float turnspeed;
     Coroutine homing;
     public float yOffset;
+
+    private float totalspeed;
+    private float totalturn;
+    public float timer = 0;
+    Vector3 targetLastPos;
     // Start is called before the first frame update
     void Start()
     {
+        totalspeed = speed;
+        totalturn = turnspeed;
         rb = GetComponent<Rigidbody>();
         players = FindObjectsOfType<Player>();
         FindClosestPlayer();
@@ -28,6 +35,19 @@ public class Projectile : Weapon
     void Update()
     {
         dir = target.position - transform.position;
+
+        //in 2 seconds make 
+        if (timer >= 1)
+        {
+            timer = 1;
+        }
+        else
+        {
+            timer = timer + Time.deltaTime;
+        }
+        speed = totalspeed * timer; //Increases time after spawn
+        //turnspeed = totalturn * (1 - timer); //decrease turn rate
+
         //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0),turnspeed * Time.deltaTime);
         //Vector3 rotationamt = Vector3.Cross(transform.forward,dir.normalized);
@@ -52,12 +72,30 @@ public class Projectile : Weapon
             }
         }
     }
-    IEnumerator Move() {
+    IEnumerator Move()
+    {
         Vector3 startPos = transform.position;
         float time = 0;
-        while(time < 3) {
-            transform.position = Vector3.Lerp(startPos, target.position + new Vector3(0,yOffset,0), time);
+        while (time < 0.5)
+        {
+            transform.position = Vector3.Lerp(startPos, target.position + new Vector3(0, yOffset, 0), time);
             transform.LookAt(target.position + new Vector3(0, yOffset, 0));
+            time += Time.deltaTime * speed;
+            targetLastPos = target.position;
+            yield return null;
+        }
+        while (time >= 0.5)
+        {
+
+            //// Calculate the direction to move (target position - current position)
+            //Vector3 moveDirection = (targetLastPos - transform.position).normalized;
+
+            //// Update the position in the move direction
+            //transform.position += Vector3.Lerp(1 * moveDirection * speed * Time.deltaTime, 60 * moveDirection * speed * Time.deltaTime, 0.5f);
+
+            Vector3 forwardMovement = Vector3.Lerp(transform.forward * speed * Time.deltaTime, 40 * transform.forward * speed * Time.deltaTime, 0.5f);
+            transform.position += forwardMovement;
+
             time += Time.deltaTime * speed;
             yield return null;
         }
